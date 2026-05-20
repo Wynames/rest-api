adad61754725be34b494bb5d12e94ddf32344827
Rey Kagami <reynotdeveloper@gmail.com>
2026-05-20 19:13:28 +0700

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,           // deteksi masalah lebih awal
+  // (opsional) di masa depan bisa ditambahkan images, i18n, dll.
+};
+
+export default nextConfig;
